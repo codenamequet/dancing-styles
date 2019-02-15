b27aaf0a8060596b927ddf920d198668a588c150
Marquet Reid <you@example.com>
2019-02-15 09:56:28 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// function registerRedirect() {
+//   const registerButton = document.querySelector('.register-button')
+//   registerButton.addEventListener('click', () => {
+//     location.href='https://www.schedulicity.com/scheduling/DSFPHW'
+//   })  
+// }
+
+// registerRedirect()

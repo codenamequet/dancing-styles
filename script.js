de0c27923448b769a37d05edc215ab4311ea17a9
marquet <marquet@element84.com>
2019-09-02 21:38:45 -0400

    added style updates and expiremented with design

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
-function registerRedirect() {
-  const registerButton = document.querySelector('.register-button')
-  registerButton.addEventListener('click', () => {
-    location.href='https://www.schedulicity.com/scheduling/DSFPHW'
-  })  
-}
+// function registerRedirect() {
+//   const registerButton = document.querySelector('.register-button')
+//   registerButton.addEventListener('click', () => {
+//     location.href='https://www.schedulicity.com/scheduling/DSFPHW'
+//   })  
+// }
 
-registerRedirect()
-
-const tvCoding = () => console.log('It\'s Lit!!! 🔥')
-
-tvCoding()
+// registerRedirect()
